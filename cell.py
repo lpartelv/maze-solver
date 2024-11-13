ea1477f831bbe537bdc6b7d1157680748581356f
Laur Pärtel Vahtra <partel@Laurs-MacBook-Air.local>
2024-11-13 14:20:28 +0200

    draw line between 2 cells

--- cell.py
+++ cell.py
@@ -14,6 +14,8 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -30,3 +32,19 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        half = abs(self._x2 - self._x1) // 2
+        x_center = half + self._x1
+        y_center = half + self._y1
+
+        half2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half2 + to_cell._x1
+        y_center2 = half2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "grey"
+        
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color=fill_color)
--- main.py
+++ main.py
@@ -6,12 +6,12 @@ def main():
     win = Window(800, 600)
 
     c = Cell(win)
-    c.has_left_wall = False
     c.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c2 = Cell(win)
+    c2.draw(125, 125, 200, 200)
+
+    c.draw_move(c2)
 
     c = Cell(win)
     c.has_bottom_wall = False
